d3f17221aa6eb34807df5d630f999dfabee583ac
JM261 <wlaks0516@gmail.com>
2023-09-01 16:38:45 +0900

    ..1

---
+++ src/test/java/com/jm/dad/DadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.dad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2e197739ec9ba38f4d4d6ab5e84e65dba7bc06f2
JM261 <wlaks0516@gmail.com>
2023-09-22 08:41:52 +0900

    ..3

--- src/main/java/com/jm/dad/user/controller/AccountController.java
+++ src/main/java/com/jm/dad/user/controller/AccountController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class MainController {
-	
-	@GetMapping("/")
-	public ModelAndView MainController(HttpServletRequest request) {
-		ModelAndView mav = new ModelAndView("thymeleaf/main");
+public class AccountController {
+
+	// 로그인 페이지
+	@GetMapping("/auth/login")
+	public ModelAndView pageLogin(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView("thymeleaf/user/account/login");
+		mav.addObject("classType", "membership login");
 		return mav;
 	}
-	
 }
--- src/test/java/com/jm/dad/DadApplicationTests.java
+++ src/test/java/com/jm/dad/DadApplicationTests.java
@@ -1,5 +1,7 @@
 package com.jm.dad;
 
+import java.util.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,6 @@ class DadApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
 
+    }
 }

30d46b13bba907c4c720e3e8628096315864b088
hjm <hjm@localhost>
2023-09-24 18:40:28 +0900

    ..6

--- src/main/java/com/jm/dad/user/controller/MainController.java
+++ src/main/java/com/jm/dad/user/controller/MainController.java
@@ -12,7 +12,7 @@ public class MainController {
 	@GetMapping("/")
 	public ModelAndView MainController(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView("thymeleaf/main");
-		System.out.println("test1");
+		System.out.println("test2");
 		return mav;
 	}
 	
